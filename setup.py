2715e92753da5075b13a26efe38cd5bc848c126a
Jordan OMara <jomara@redhat.com>
2013-10-03 17:14:21 -0400

    Setup + data

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='AskUX',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='AskBot for OpenStack UX',
+      author='Jordan OMara',
+      author_email='jomara@redhat.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Django>=1.4'],
      )
